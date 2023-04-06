8e3112066fae19693bc66b0322abefb9b40a9e98
Shweta Nimbalkar <122146810+Shweta-nimbalkar@users.noreply.github.com>
2023-04-06 20:40:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+
+for (let i = 0; i < 101; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+
+        // console.log("FizzBuzz");
+        document.write("FizzBuzz" + "<br>");
+
+    } else if (i % 3 === 0) {
+
+        // console.log("Buzz");
+        document.write("Buzz" + "<br>");
+
+    } else if (i % 5 === 0) {
+
+        // console.log("Fizz");
+        document.write("Fizz" + "<br>");
+
+    }
+
+}
